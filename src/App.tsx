6c616e580fefe423316444853513ccaf8602e3a1
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-07-11 23:38:47 +0200

    feat: implement API search call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import TopSection from './components/TopSection';
 
-class App extends React.Component {
-  handleSearch = (searchTerm: string) => {
-    localStorage.setItem('searchTerm', searchTerm);
+interface Person {
+  name: string;
+  birth_year: string;
+  gender: string;
+}
+
+interface State {
+  results: Person[];
+}
+
+class App extends React.Component<Record<string, never>, State> {
+  state = {
+    results: [],
+  };
+
+  handleSearch = async (searchTerm: string) => {
+    const url = searchTerm
+      ? `https://swapi.py4e.com/api/people/?search=${encodeURIComponent(searchTerm)}`
+      : 'https://swapi.py4e.com/api/people/';
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const results: Person[] = data.results;
+
+      this.setState({ results }, () => console.log(this.state.results));
+
+      localStorage.setItem('searchTerm', searchTerm);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
