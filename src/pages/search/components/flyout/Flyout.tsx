6bd6e45eb72ca821df546367238ad09096e46d43
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-07-31 11:00:04 +0200

    feat: add Unselect all button to Flyout

--- src/pages/search/components/flyout/Flyout.tsx
+++ src/pages/search/components/flyout/Flyout.tsx
@@ -3,12 +3,16 @@ import { useSelectionStore } from '#/shared/store/useSelectionStore.ts';
 export function Flyout() {
   console.log('Flyout mounted');
   const selected = useSelectionStore((state) => state.selected);
+  const clearSelection = useSelectionStore((state) => state.clearSelection);
 
   if (selected.length === 0) return null;
 
   return (
     <div className={'flyout'}>
       <span>{selected.length} items are selected</span>
+      <div>
+        <button onClick={clearSelection}>Unselect all</button>
+      </div>
     </div>
   );
 }
