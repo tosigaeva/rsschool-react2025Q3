f23ff29605ba5fd08cec0ea57455c99f363e2d88
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-07-31 16:37:23 +0200

    feat: add CSV Download button to Flyout

--- src/pages/search/components/flyout/Flyout.tsx
+++ src/pages/search/components/flyout/Flyout.tsx
@@ -1,4 +1,6 @@
 import { useSelectionStore } from '#/shared/store/useSelectionStore.ts';
+import { downloadCsv, generateCsvContent } from '#/shared/utils/csv.ts';
+import type { Character } from '#/types';
 
 export function Flyout() {
   console.log('Flyout mounted');
@@ -7,11 +9,21 @@ export function Flyout() {
 
   if (selected.length === 0) return null;
 
+  const handleDownload = () => {
+    //TODO: Decide whether to include all fields or only specific ones
+    //const headers: (keyof Character)[] = ['name', 'birth_year', 'gender', 'url'];
+    const headers = Object.keys(selected[0]) as (keyof Character)[];
+    const csvContent = generateCsvContent(headers, selected);
+    const fileName = `${selected.length}_items.csv`;
+    downloadCsv(fileName, csvContent);
+  };
+
   return (
     <div className={'flyout'}>
       <span>{selected.length} items are selected</span>
       <div>
         <button onClick={clearSelection}>Unselect all</button>
+        <button onClick={handleDownload}>Download</button>
       </div>
     </div>
   );
