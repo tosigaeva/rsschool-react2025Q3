3a88b7e5c546ec5c4b23aedfa49ac55913756de5
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-03 17:04:36 +0200

    refactor: remove direct DOM manipulation from CSV download

--- src/pages/search/components/flyout/Flyout.tsx
+++ src/pages/search/components/flyout/Flyout.tsx
@@ -1,21 +1,45 @@
 import { useSelectionStore } from '#/shared/store/useSelectionStore.ts';
-import { downloadCsv, generateCsvContent } from '#/shared/utils/csv.ts';
 import type { Character } from '#/types';
+import { useRef, useState } from 'react';
 
 export function Flyout() {
-  console.log('Flyout mounted');
   const selected = useSelectionStore((state) => state.selected);
   const clearSelection = useSelectionStore((state) => state.clearSelection);
 
+  const downloadRef = useRef<HTMLAnchorElement | null>(null);
+  const [downloadUrl, setDownloadUrl] = useState('');
+
   if (selected.length === 0) return null;
 
+  const generateCsvContent = (
+    headers: (keyof Character)[],
+    rows: Character[]
+  ): string => {
+    const rowLines = rows.map((row) => headers.map((head) => row[head]));
+    return [headers.join(','), ...rowLines].join('\n');
+  };
+
   const handleDownload = () => {
-    //TODO: Decide whether to include all fields or only specific ones
-    //const headers: (keyof Character)[] = ['name', 'birth_year', 'gender', 'url'];
-    const headers = Object.keys(selected[0]) as (keyof Character)[];
+    const headers: (keyof Character)[] = [
+      'name',
+      'birth_year',
+      'gender',
+      'url',
+    ];
     const csvContent = generateCsvContent(headers, selected);
-    const fileName = `${selected.length}_items.csv`;
-    downloadCsv(fileName, csvContent);
+
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    setDownloadUrl(url);
+
+    setTimeout(() => {
+      if (downloadRef.current) {
+        downloadRef.current.click();
+        URL.revokeObjectURL(url);
+        setDownloadUrl('');
+      }
+    }, 0);
   };
 
   return (
@@ -25,6 +49,14 @@ export function Flyout() {
         <button onClick={clearSelection}>Unselect all</button>
         <button onClick={handleDownload}>Download</button>
       </div>
+      <a
+        ref={downloadRef}
+        href={downloadUrl}
+        download={`${selected.length}_items.csv`}
+        style={{ display: 'none' }}
+      >
+        Download
+      </a>
     </div>
   );
 }
