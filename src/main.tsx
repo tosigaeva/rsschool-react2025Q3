cb20f5ccfcdfa452e915306beb15985ada816e70
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-08 21:10:23 +0200

    feat: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router';
 import { ErrorBoundary } from '#/pages/search/components/error-boundary';
 import { ThemeProvider } from '#/shared/theme/provider.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '#/shared/api/queryClient.ts';
 
 const rootElement = document.getElementById('root');
 
@@ -16,9 +18,11 @@ createRoot(rootElement).render(
   <StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
