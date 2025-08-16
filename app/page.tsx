29d8788f6c7de54301b76472d9c3d2c985cff66c
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-16 22:44:26 +0200

    feat: add search-panel to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-    return <div>Search page</div>
+import SearchPanel from '@/ui/search-panel';
+
+export default async function Page() {
+  return (
+    <main className="relative flex flex-col items-center mx-auto my-0 max-w-7xl p-5">
+      <SearchPanel />
+    </main>
+  );
 }
