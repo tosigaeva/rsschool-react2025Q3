4723e56039329eb91469cab59fb8a23139a5c4dd
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-18 02:17:17 +0200

    fix deploy

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import type { InputProps } from "#/shared/ui/props/inputProps.ts";
+import {InputProps} from "#/components/ui/props/inputProps";
 
 export function Input({ value, onChange, placeholder }: InputProps) {
   return (
