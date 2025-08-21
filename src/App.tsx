ca3cb074ee950afd5c0c4f701aedaf271b1c1e22
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-21 22:48:48 +0200

    feat: add header

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>React Forms</h1>;
-}
-
-export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex items-center justify-between bg-white px-8 py-4 shadow-2xs">
+      <h1 className="my-5 text-3xl font-bold">React Forms</h1>
+    </header>
+  );
+}
