a87face470518a0b21ae5642bb74b1c5e67cde81
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-22 17:29:23 +0200

    refactor: extract portal into component

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { Portal } from '@/components/portal';
 
 type Props = {
   children?: React.ReactNode;
@@ -7,17 +7,13 @@ type Props = {
 };
 export function Modal({ children, onClose, open }: Props) {
   if (!open) return null;
-
-  const portalElement = document.getElementById('portal');
-  if (!portalElement) return null;
-  return ReactDOM.createPortal(
-    <>
+  return (
+    <Portal>
       <div className="fixed inset-0 z-10 bg-gray-900/80" />
       <div className="fixed top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 bg-white p-10">
         <button onClick={onClose}>x</button>
         {children}
       </div>
-    </>,
-    portalElement
+    </Portal>
   );
 }
