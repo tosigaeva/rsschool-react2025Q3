e6c17f1337195a126ad5a46f7d7787c3aab89543
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-08-23 15:23:12 +0200

    feat: integrate hook-form with schema

--- src/components/forms/hook-form/hook-form.tsx
+++ src/components/forms/hook-form/hook-form.tsx
@@ -1,59 +1,101 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
 import { FormField } from '@/components/forms';
 import { Button, Checkbox, Input, Select } from '@/components/ui';
+import { type FormData, schema } from '@/shared/validation-schema';
 
 export function HookForm() {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormData>({
+    mode: 'onChange',
+    resolver: zodResolver(schema),
+  });
+
+  const submitData = (data: FormData) => {
+    console.log('Form data:', data);
+  };
   return (
     <>
       <h2 className="mb-6 text-center font-bold">React Hook Form</h2>
-      <form className="flex flex-col gap-4">
-        <FormField error="Name is required" id="name" label="Name">
-          <Input id="name" type="text" />
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(submitData)}>
+        <FormField error={errors.name?.message} id="name" label="Name">
+          <Input id="name" type="text" {...register('name')} />
         </FormField>
 
-        <FormField id="age" label="Age">
-          <Input id="age" type="number" />
+        <FormField error={errors.age?.message} id="age" label="Age">
+          <Input
+            id="age"
+            type="number"
+            {...register('age', { valueAsNumber: true })}
+          />
         </FormField>
 
-        <FormField id="email" label="Email">
-          <Input id="email" type="email" />
+        <FormField error={errors.email?.message} id="email" label="Email">
+          <Input id="email" type="email" {...register('email')} />
         </FormField>
 
-        <FormField id="password" label="Password">
-          <Input id="password" type="password" />
+        <FormField
+          error={errors.password?.message}
+          id="password"
+          label="Password"
+        >
+          <Input id="password" type="password" {...register('password')} />
         </FormField>
 
-        <FormField id="confirmPassword" label="Confirm password">
-          <Input id="confirmPassword" type="password" />
+        <FormField
+          error={errors.confirmPassword?.message}
+          id="confirmPassword"
+          label="Confirm password"
+        >
+          <Input
+            id="confirmPassword"
+            type="password"
+            {...register('confirmPassword')}
+          />
         </FormField>
 
-        <FormField id="gender" label="Gender">
-          <Select id="gender">
-            <option>Male</option>
-            <option>Female</option>
+        <FormField error={errors.gender?.message} id="gender" label="Gender">
+          <Select id="gender" {...register('gender')}>
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
           </Select>
         </FormField>
 
-        <FormField error="You must accept terms" id="tc" label="">
+        <FormField error={errors.terms?.message} id="terms" label="">
           <div className="flex items-center gap-2">
-            <Checkbox id="tc" />
-            <label className="text-sm" htmlFor="tc">
+            <Checkbox id="terms" {...register('terms')} />
+            <label className="text-sm" htmlFor="terms">
               Accept Terms & Conditions
             </label>
           </div>
         </FormField>
 
-        <FormField id="picture" label="Upload Picture">
-          <Input accept="image/png, image/jpeg" id="picture" type="file" />
+        <FormField
+          error={errors.picture?.message}
+          id="picture"
+          label="Upload Picture"
+        >
+          <Input
+            accept="image/png, image/jpeg"
+            id="picture"
+            type="file"
+            {...register('picture')}
+          />
         </FormField>
 
-        <FormField id="country" label="Country">
-          <Input id="country" />
+        <FormField error={errors.country?.message} id="country" label="Country">
+          <Input id="country" {...register('country')} />
         </FormField>
 
         <Button
           className="mt-6 w-full bg-green-300"
-          onClick={() => {}}
-          text={'Submit'}
+          text="Submit"
+          type="submit"
         />
       </form>
     </>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,14 +1,18 @@
-type Props = {
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className: string;
-  onClick: () => void;
+  onClick?: () => void;
   text: string;
 };
 
-export function Button({ className, onClick, text }: Props) {
+export function Button({ className, onClick, text, ...props }: Props) {
   const defaultClassName =
     'py-2 px-5 border-none rounded-lg cursor-pointer font-bold text-white  w-60';
   return (
-    <button className={`${defaultClassName} ${className}`} onClick={onClick}>
+    <button
+      className={`${defaultClassName} ${className}`}
+      onClick={onClick}
+      {...props}
+    >
       {text}
     </button>
   );
